34130e0b451a3acd68eaedbbc7f824ecad7ed621
Danny <dantran604@gmail.com>
2024-12-06 20:12:08 -0800

    created product options for single page

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,33 @@
 import Image from 'next/image'
 import { useState } from 'react'
 
-const images = [
-  {
-    id: 1,
-    url: 'https://images.pexels.com/photos/14643689/pexels-photo-14643689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-  },
-  {
-    id: 2,
-    url: 'https://images.pexels.com/photos/29549363/pexels-photo-29549363/free-photo-of-vibrant-european-robin-perched-in-nature.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-  },
-  {
-    id: 3,
-    url: 'https://images.pexels.com/photos/16475273/pexels-photo-16475273/free-photo-of-urban-yellow-colonnade.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-  },
-  {
-    id: 4,
-    url: 'https://images.pexels.com/photos/29321623/pexels-photo-29321623/free-photo-of-picturesque-street-view-of-dresden-s-historic-architecture.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-  }
-]
+// const images = [
+//   {
+//     id: 1,
+//     url: 'https://images.pexels.com/photos/14643689/pexels-photo-14643689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+//   },
+//   {
+//     id: 2,
+//     url: 'https://images.pexels.com/photos/29549363/pexels-photo-29549363/free-photo-of-vibrant-european-robin-perched-in-nature.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+//   },
+//   {
+//     id: 3,
+//     url: 'https://images.pexels.com/photos/16475273/pexels-photo-16475273/free-photo-of-urban-yellow-colonnade.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+//   },
+//   {
+//     id: 4,
+//     url: 'https://images.pexels.com/photos/29321623/pexels-photo-29321623/free-photo-of-picturesque-street-view-of-dresden-s-historic-architecture.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+//   }
+// ]
 
-const ProductImages = () => {
+const ProductImages = ({items}:{items: any}) => {
   const [index, setIndex] = useState(0)
 
   return (
     <div className=''>
       <div className='h-[500px] relative'>
         <Image
-          src={images[index].url}
+          src={items[index].image?.url}
           alt=''
           fill
           sizes='30vw'
@@ -37,14 +37,14 @@ const ProductImages = () => {
         />
       </div>
       <div className='flex justify-between gap-4 mt-8'>
-        {images.map((img,i) => (
+        {items.map((item: any, i: number) => (
           <div 
             className='w-1/4 h-32 relative gap-4 mt-8 cursor-pointer' 
-            key={img.id}
+            key={item._id}
             onClick={() => setIndex(i)}>
 
             <Image
-              src={img.url}
+              src={item.image?.url}
               alt=''
               fill
               sizes='30vw'
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,118 +1,74 @@
-import Image from "next/image";
-import Link from "next/link";
+import { wixClientServer } from '@/lib/wixClientServer'
+import { products } from '@wix/stores'
+import Image from 'next/image'
+import Link from 'next/link'
+import  DOMPurify from 'isomorphic-dompurify'
 
-const ProductList = () => {
+const PRODUCT_PER_PAGE = 20
 
-    return (
-        <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
-                <div className="relative w-full h-80">
-                    
-                    <Image 
-                        src='https://images.pexels.com/photos/20881841/pexels-photo-20881841/free-photo-of-big-beautiful-flower-blooming.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                        />
-                    <Image 
-                        src='https://images.pexels.com/photos/19457056/pexels-photo-19457056/free-photo-of-tables-and-chairs-standing-outside-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md"
-                        />
-                </div>
-                <div className="flex justify-between">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">$49</span>
-                </div>
-                <div className="text-sm text-gray-500">My description</div>
-                <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white
-                ">Add to Cart</button>
-            </Link>
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
-                <div className="relative w-full h-80">
-                    
-                    <Image 
-                        src='https://images.pexels.com/photos/20881841/pexels-photo-20881841/free-photo-of-big-beautiful-flower-blooming.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                        />
-                    <Image 
-                        src='https://images.pexels.com/photos/19457056/pexels-photo-19457056/free-photo-of-tables-and-chairs-standing-outside-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md"
-                        />
-                </div>
-                <div className="flex justify-between">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">$49</span>
-                </div>
-                <div className="text-sm text-gray-500">My description</div>
-                <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white
-                ">Add to Cart</button>
-            </Link>
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
-                <div className="relative w-full h-80">
-                    
-                    <Image 
-                        src='https://images.pexels.com/photos/20881841/pexels-photo-20881841/free-photo-of-big-beautiful-flower-blooming.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                        />
-                    <Image 
-                        src='https://images.pexels.com/photos/19457056/pexels-photo-19457056/free-photo-of-tables-and-chairs-standing-outside-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md"
-                        />
-                </div>
-                <div className="flex justify-between">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">$49</span>
-                </div>
-                <div className="text-sm text-gray-500">My description</div>
-                <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white
-                ">Add to Cart</button>
-            </Link>
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
-                <div className="relative w-full h-80">
-                    
-                    <Image 
-                        src='https://images.pexels.com/photos/20881841/pexels-photo-20881841/free-photo-of-big-beautiful-flower-blooming.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                        />
-                    <Image 
-                        src='https://images.pexels.com/photos/19457056/pexels-photo-19457056/free-photo-of-tables-and-chairs-standing-outside-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                        alt=""
-                        fill
-                        sizes="25vw"
-                        className="absolute object-cover rounded-md"
-                        />
-                </div>
-                <div className="flex justify-between">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">$49</span>
-                </div>
-                <div className="text-sm text-gray-500">My description</div>
-                <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white
-                ">Add to Cart</button>
-            </Link>
-            
-        </div>
-      )
-    }
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string
+  limit?: number
+  searchParams?: any
+}) => {
+  const wixClient = await wixClientServer()
+  const res = await wixClient.products
+    .queryProducts()
+    .eq('collectionIds', categoryId)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .find()
 
 
-    export default ProductList;
+
+  return (
+    
+<div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
+{res.items.map((product: products.Product) => (
+<Link
+    href={'/'+product.slug}
+    className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]' key={product._id}
+    >
+    <div className='relative w-full h-80'>
+    <Image
+        src={product.media?.mainMedia?.image?.url || "/product.png"}
+        alt=''
+        fill
+        sizes='25vw'
+        className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
+        />
+    {product.media?.items && (
+        <Image
+        src={product.media?.items[1]?.image?.url || "/product.png"}
+        alt=''
+        fill
+        sizes='25vw'
+        className='absolute object-cover rounded-md'
+        />
+    )}
+    </div>
+    <div className='flex justify-between'>
+    <span className='font-medium'>{product.name}</span>
+    <span className='font-semibold'>{product.price?.price}</span>
+    </div>
+    <div className='text-sm text-gray-500'dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(product.additionalInfoSections?.find((section:any)=>section.title === "shortDesc")?.description || "")}}>
+        
+    </div>
+    <button
+    className='rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white
+            '
+            >
+    Add to Cart
+    </button>
+</Link>
+    ))};
+
+</div>
+   
+  )
+}
+
+export default ProductList
